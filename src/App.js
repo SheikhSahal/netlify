901e984de669d4caefd840e5d99a78a5ff498837
Muhammad Sahal Qasim <36259752+SheikhSahal@users.noreply.github.com>
2021-01-07 15:59:40 +0500

    deploy funtion on netlify

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
- import React from 'react';
+ import React, { useEffect, useState } from 'react';
 
 import './App.css';
-// , { useEffect, useState }
 function App() {
 
-  // const [val, setVal] = useState({});
+  const [val, setVal] = useState({});
 
-  // useEffect(()=>{
-  //   fetch(`/.netlify/functions/hello?name=from new react app`)
-  //   .then(res=>res.json())
-  //   .then(obj=>setVal(obj))
-  // },[])
+  useEffect(()=>{
+    fetch(`/.netlify/functions/hello?name=from new react app`)
+    .then(res=>res.json())
+    .then(obj=>setVal(obj))
+  },[])
+
   return (
     <div className="App">
       Hellow world from update.
-      {/* <h3>{val.message}</h3> */}
+      <h3>{val.message}</h3>
     </div>
   );
 }
